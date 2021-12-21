b91c3174ed5c05c962cd5cf0a92b0bd6c8f546c2
zeynepiskndr <zeynep.iskender97@gmail.com>
2021-12-21 14:24:44 +0300

    Update main_ctrl.py

--- controllers/main_ctrl.py
+++ controllers/main_ctrl.py
@@ -5,7 +5,6 @@ import os
 from model.model import Model
 from views.main_view import MainView
 
-from PyQt5 import QtGui
 from PyQt5.QtCore import QObject
 
 class MainController(QObject):
@@ -17,16 +16,10 @@ class MainController(QObject):
         self._view.show()
 
     def select_video_controller(self):
-        """
-        select video butonuna basıldıktan sonra videonun Pathini viewdan alıp path olarak dosyanın pathini buluyor
-        """
+    
         self._model.running = True
 
         path_name = self._view.select_video()
-
-        head_tail = os.path.split(path_name)
-        self._model.path = head_tail[0]
-        self._model.file_name = head_tail[1]
 
         self._model.set_path(path_name)
 
@@ -36,9 +29,7 @@ class MainController(QObject):
             self._model.first_video_image(path_name)
 
     def play_video_controller(self):
-        """
-        run butonuna basıldıktan sonra mdeldeki sumVideoyu çalıştırabilmek için thread oluşturuyor sonra viewa gidip sonuçları gösteriyor
-        """
+        
         if self._model.running == False:
             self._model.reset_video_variables()
         else:
@@ -47,24 +38,18 @@ class MainController(QObject):
             self._view.play_video()
 
     def show_beam_sum_image(self):
-        """
-        run butonuna basılması sonucunda dönen imageları viewda gösteriyor
-        """
+        
         self._view.show_video_images()
 
     def stop_video_controller(self):
-        """
-        stop utonuna basılıca videonun durmasını sağlıyor
-        """
+        
         self._model.stop_flag()
         if self._model.running == False:
             self._view.stop_video()
             self._model.reset_video_variables()
 
     def thresh_image_controller(self, seg_tresh):
-        """
-        Execute->threshold kısmından seçilen değerin alınması
-        """
+        
         self._view.start_gif()
         self._view.start_animation()
         threshold_thread = threading.Thread(target = self._model.threshold_thread, args=(seg_tresh,))#thread for threshold
@@ -77,9 +62,7 @@ class MainController(QObject):
         self._view.stop_animation()
     
     def show_first_image(self):
-        """
-        videodaki ilk frame viewda gösteriliyor
-        """
+        
         self._view.show_first_thresh_image()
     
     def show_second_image(self):
@@ -117,4 +100,5 @@ class MainController(QObject):
     #SET FUNCTIONS
     def set_progresbar(self, count):
         self._view.set_progresbar(count)
+
 
