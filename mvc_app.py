694ad6421ba68dbeb0f18db95b3cb0255714f24f
zeynepiskndr <zeynep.iskender97@gmail.com>
2022-01-04 16:02:21 +0300

    Update mvc_app.py

--- mvc_app.py
+++ mvc_app.py
@@ -1,15 +1,15 @@
 import sys
-from controllers.main_ctrl import MainController
-
 from PyQt5.QtWidgets import QApplication
 
-#script from which the application is run 
+from controllers.main_ctrl import MainController
+
 class App(QApplication):
     def __init__(self, sys_argv):
         super(App, self).__init__(sys_argv)
-        
+
         self.controller = MainController()
+
 #main
 if __name__ == '__main__':
     app = App(sys.argv)
-    sys.exit(app.exec_()) 
+    sys.exit(app.exec_())
