97c771ee26a6243c858bbaeecd12d836a288659f
zeynepiskndr <zeynep.iskender97@gmail.com>
2022-01-04 16:02:15 +0300

    Update main_ctrl.py

--- controllers/main_ctrl.py
+++ controllers/main_ctrl.py
@@ -1,27 +1,14 @@
-#from _typeshed import Self
-from math import e
-from sys import path
-import threading
-import time
-import os
-
-from numpy.lib.function_base import select
-
-from model.model import Model
+from model.model import Model, Worker, Worker2
 from views.main_view import MainView
-
-from PyQt5.QtCore import QObject
-
 import model.utils as utils
 
-class MainController(QObject):
+class MainController():
+
     def __init__(self):
-        super().__init__()
-        """
-        connecting from controller to view and model
-        """
+
         self._model = Model(self)
         self._view = MainView(self)
+
         self._view.show()
 
     def select_video_controller(self):
@@ -32,17 +19,12 @@ class MainController(QObject):
 
         path_name = self._view.select_video()
         self._model.set_path(path_name)
-
         if path_name != '':
-            self._model.first_video_image(path_name)
-        else:
-            self._view.warning_video()
+            worker = Worker(self._model.first_video_image)
+            worker.signals.result.connect(self._view.show_first_thresh_image)
 
-    def run_again(self):
-        self._model.running = True
-        path_name = self._model.Path
-        if path_name != '':
-            self._model.first_video_image(path_name)
+            # Execute
+            self._model.threadpool.start(worker)
         else:
             self._view.warning_video()
 
@@ -50,72 +32,43 @@ class MainController(QObject):
         """
         After pressing the run button, it creates a thread to run the sum_video in the model, then goes to the view and shows the results.
         """
-        if self._model.text == False: 
-            video_thread = threading.Thread(target = self._model.frame_sum_thread, args=(exp_time, beam_thresh, ref_point_thresh,))#thread for frame_sum
-            video_thread.start()
-            self._view.play_video()
-        else:
-            self.run_again()
-            video_thread = threading.Thread(target = self._model.frame_sum_thread, args=(exp_time, beam_thresh, ref_point_thresh,))#thread for frame_sum
-            video_thread.start()
-            self._view.play_video()
+        self._model.running = True
 
-        #if self._model.running == False:
-            #self._view.clear_all_results()
+        self._model.set_exp_time(exp_time)
+        self._model.set_beam_thresh(beam_thresh)
+        self._model.set_res_point_thresh(ref_point_thresh)
+
+        worker = Worker2(self._model.sum_video)
+        worker.signals.result.connect(self._view.show_video_images)
+        worker.signals.finished.connect(self._view.thread_complete)
+        worker.signals.progress.connect(self._view.set_progressbar)
+        worker.signals.state.connect(self._view.after_5_sn_img)
+        worker.signals.stop.connect(self._view.stop_video)
         
-        #self._view.warning_video()
+        # Execute
+        self._model.threadpool.start(worker)
+        self._view.play_video()
 
-    def show_beam_sum_image(self):
-        """
-        Shows the images returned as a result of pressing the run button.
-        """
-        if self._model.beam_sum_img != None:
-            self._view.show_video_images()
-            self._model.text = True
-            
     def stop_video_controller(self):
         """
         It makes the video stop when the stop button is pressed.
         """
         self._model.stop_flag()
-        if self._model.running == False:
-            self._view.stop_video()
-            self._view.clear_all_results()
 
     def thresh_image_controller(self, seg_tresh):
         """
         Getting the value selected from the Execute->threshold section
         """
-        self._view.start_gif()
-        self._view.start_gif2_animation()
-        threshold_thread = threading.Thread(target = self._model.threshold_thread, args=(seg_tresh,))#thread for threshold
-        threshold_thread.start()
+        self._model.set_seg_tresh(seg_tresh)
 
-    def thresh_image_results(self):
-        """
-        Images obtained after threshold is set are displayed in view
-        """
-        self._view.show_thresh_images()
-        self._view.stop_gif2_animation()
-    
-    def show_first_image(self):
-        """
-        the first frame view in the video is also shown
-        """
-        self._view.show_first_thresh_image()
-    
-    def show_second_image(self):
-        """
-        The image showing the intersections of the points of the lines obtained by combining the points in the first frame.
-        """
-        self._view.show_second_image()
-
-    def warning_video_taken(self):
-        """
-        If the function cannot find the points in the first 5 seconds, it prints a warning in the view and shot down the application.
-        """
-        self._view.warning_video_taken()
-        #self._view.close_app()
+        worker = Worker(self._model.threshold_thread)
+        worker.signals.result.connect(self._view.show_thresh_images)
+        worker.signals.finished.connect(self._view.stop_gif)
+        worker.signals.stop.connect(self._view.warning_msg)
+        
+        # Execute
+        self._model.threadpool.start(worker)
+        self._view.after_select_thresh()
 
     def save_to_file(self):
         date = utils.make_date()
@@ -129,10 +82,12 @@ class MainController(QObject):
 
         utils.save_to_file(file, "\n\nError Informationn\nLeft Error mm: " + str(self._model.x_mm))
         utils.save_to_file(file, "\nAnterior Error mm (A/L) Image: " + str(self._model.y_mm))
-        utils.save_to_file(file, "\nSuperior Error mm:: " + str(self._model.x_mm))
+        utils.save_to_file(file, "\nSuperior Error mm: " + str(self._model.x_mm))
         utils.save_to_file(file, "\nAnterior Error mm (A/S) Image: " + str(self._model.y_mm))
         utils.save_to_file(file, "\nAverage Anterior Error mm: " + str(self._model.avr_ant_err))
         utils.save_to_file(file, "\nTOTAL TARGETING ERROR mm: " + str(self._model.target_err))
+
+        self._view.warning_save_variables()
 
     #GET FUNCTIONS
     def get_x_mm(self):
@@ -147,31 +102,11 @@ class MainController(QObject):
     def get_targetting_err(self):
         return self._model.target_err
 
-    def get_success(self):
-        return self._model.success
-
     def get_running(self):
         return self._model.running
-    
-    def get_text_flag(self):
-        return self._model.text
-
-    def get_path(self):
-        return self._model.Path
 
     def get_first_frame(self):
         return self._model.image_qt
 
     def get_second_frame(self):
         return self._model.drawing_img
-
-    def get_binary_img(self):
-        return self._model.binary_img_qt
-
-    def get_beam_sum_img(self):
-        return self._model.beam_sum_img
-
-    #SET FUNCTIONS
-    def set_progresbar(self, count):
-        self._view.set_progresbar(count)
-    
