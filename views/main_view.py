9a38b0183107e81d065c7aa8849b2b22c9ae204d
zeynepiskndr <zeynep.iskender97@gmail.com>
2022-01-04 16:02:29 +0300

    Update main_view.py

--- views/main_view.py
+++ views/main_view.py
@@ -1,28 +1,22 @@
 import sys
 import os
-from PyQt5 import QtGui
+import time
 from PyQt5.QtGui import QPixmap, QMovie
 #importing necessary widgets
 from PyQt5.QtWidgets import (QMainWindow, QMessageBox, QFileDialog, QStyle)
+
 #load view
 from views.main_view_ui3 import Ui_MainWindow
 
-
-#All widgets that appear in Qt designer are in this section.
 class MainView(QMainWindow):
-    def __init__(self, controller):
-        """
-        Defining and linking widgets that require action that appear in the gui
-        """
-        super().__init__()
-        
-        #view controller connection 
-        self._controller = controller
 
+    def __init__(self, controller):
+        super(MainView, self).__init__()
+
+        self._controller = controller
         self._ui = Ui_MainWindow()
         self._ui.setupUi(self)
 
-        # click buttons & connect widgets to controller
         self._ui.select_video.clicked.connect(self._controller.select_video_controller)
         self._ui.run_video.setIcon(self.style().standardIcon(QStyle.SP_MediaPlay))
         self._ui.run_video.clicked.connect(lambda: self._controller.play_video_controller(self._ui.combo_box_3.currentText(),
@@ -30,11 +24,12 @@ class MainView(QMainWindow):
                                                                                         self._ui.combo_box_2.currentText()))
         self._ui.stop_video.setIcon(self.style().standardIcon(QStyle.SP_MediaStop))
         self._ui.stop_video.clicked.connect(self._controller.stop_video_controller)
-        self._ui.clear.clicked.connect(self.clear_all_results)   
-     
+        self._ui.clear.clicked.connect(self.clear_all_results) 
+        
         #menu bar triggered
         self._ui.Exit.triggered.connect(self.close_app)
         self._ui.Save_TXT_File.triggered.connect(self._controller.save_to_file)
+        self._ui.actionAbout.triggered.connect(self.warning_help_about)
 
         #threshold buttons
         self._ui.action60.triggered.connect(lambda: self._controller.thresh_image_controller(60))
@@ -45,55 +40,29 @@ class MainView(QMainWindow):
 
         #for the application to start from the video page
         self._ui.tabwidget.setCurrentIndex(0)
-        self._ui.progressBar.hide()
         self._ui.clear.hide()
 
         #set default variables
         self._ui.combo_box_1.setCurrentIndex(4)
         self._ui.combo_box_2.setCurrentIndex(5)
         self._ui.combo_box_3.setCurrentIndex(1)
-    
+
         # determine if application is a script file or frozen exe
         image_name = 'Spinner.gif'
         if getattr(sys, 'frozen', False):
             application_path = os.path.dirname(sys.executable)
         elif __file__:
             application_path = os.path.dirname(__file__)
-
         image_path = os.path.join(application_path, image_name)
-
-        self.movie1 = QMovie(image_path)
-        self.movie2 = QMovie(image_path)
-
-    def start_gif1_animation(self):   
-        """
-        Start Gif Animation
-        """     
-        self.movie1.start()
-
-    def start_gif2_animation(self):
-        """
-        Start Gif Animation
-        """ 
-        self.movie2.start()
-  
-    def stop_gif1_animation(self):
-        """
-        Start Gif Animation
-        """ 
-        self.movie1.stop()
-
-    def stop_gif2_animation(self):
-        """
-        Start Gif Animation
-        """ 
-        self.movie2.stop()
+        self.movie = QMovie(image_path)
 
     def select_video(self):
         """
         When you press the select button, it shows the things that need to be changed on the screen.            
         """
-        #self._ui.progressBar.setValue(0)
+        self._ui.progressbar.setValue(0)
+        self._ui.execute.setEnabled(False)
+        self._ui.menuFile.setEnabled(False)
 
         self._ui.g_image_1.clear()
         self._ui.g_image_2.clear()
@@ -104,16 +73,12 @@ class MainView(QMainWindow):
         self._ui.T1_label_3.clear()
         self._ui.T1_label_4.clear()
         self._ui.T2_label_3.clear()
-        self._ui.T2_label_4.clear()
+        self._ui.T2_label_4.clear()        
 
         path_name, _ = QFileDialog.getOpenFileName(self,"Video Files")
+        self._ui.run_video.setEnabled(True)
 
-        if path_name != '':
-            self._ui.run_video.setEnabled(True)
-        #else:
-            #self.warningMsg()
-
-        return path_name
+        return path_name     
 
     def play_video(self):
         """
@@ -123,104 +88,122 @@ class MainView(QMainWindow):
         self._ui.stop_video.setEnabled(True)
         self._ui.run_video.setEnabled(False)
         self._ui.select_video.setEnabled(False)
-        self._ui.progressing.setText("Executing!")
-
-        self._ui.gif_1.setMovie(self.movie1)
-    
-        self.start_gif1_animation()
+        self._ui.execute.setEnabled(False)
+        self._ui.menuFile.setEnabled(False)
 
         self._ui.combo_box_1.setEnabled(False)
         self._ui.combo_box_2.setEnabled(False)
         self._ui.combo_box_3.setEnabled(False)
+        
+        self._ui.player.setPixmap(self._controller.get_first_frame())
+        #self._ui.player.setPixmap(self._controller.get_second_frame())
 
-        #self.clear_all_results()# farklı bir fonksiyon yaz ya da ihtiyaca bak
-
-    def show_video_images(self):
-        """
-        When the run button is pressed, it shows the last things that need to be changed on the screen.
-        """
-        self._ui.execute.setEnabled(True)
-        self._ui.progressing.setText("Completed!")
-        self.stop_gif1_animation()
-
-        self._ui.tabwidget.setCurrentIndex(1)
-
-        self._ui.g_image_1.setPixmap(QtGui.QPixmap((self._controller.get_beam_sum_img())))
-        self._ui.g_image_2.setPixmap(QtGui.QPixmap((self._controller.get_beam_sum_img())))
-
-        self._ui.select_video.setEnabled(True)
-        self._ui.run_video.setEnabled(True)
-        self._ui.stop_video.setEnabled(False) 
-
-        self._ui.combo_box_1.setEnabled(True)
-        self._ui.combo_box_2.setEnabled(True)
-        self._ui.combo_box_3.setEnabled(True)
-
-    def set_progresbar(self, count):
-        """
-        Written for progress bar but not used
-        """
-        self._ui.progressBar.setValue(count)
-                
-    def stop_video(self):
+    def stop_video(self, common_flag):
         """
         It shows the things that need to change on the screen when the stop button is pressed.
         """
-        self._ui.progressing.setText("Stopped!")
+        if common_flag == 0:
+            print(common_flag)
+            self._ui.progressbar.setValue(0)
 
-        self._ui.select_video.setEnabled(True)
-        self._ui.run_video.setEnabled(False)
-        self._ui.stop_video.setEnabled(False)
+            self._ui.select_video.setEnabled(True)
+            self._ui.run_video.setEnabled(True)
+            self._ui.stop_video.setEnabled(False)
+            self._ui.execute.setEnabled(False)
+            self._ui.menuFile.setEnabled(False)
 
-        self._ui.g_image_1.clear()
-        self._ui.g_image_2.clear()
+            self._ui.combo_box_1.setEnabled(True)
+            self._ui.combo_box_2.setEnabled(True)
+            self._ui.combo_box_3.setEnabled(True)
 
-        self.stop_gif1_animation()
+            self.clear_all_results()
 
-    def start_gif(self):
+        else:
+            print(common_flag)
+            self._ui.progressbar.setValue(0)
+
+            self._ui.select_video.setEnabled(True)
+            self._ui.run_video.setEnabled(True)
+            self._ui.stop_video.setEnabled(False)
+            self._ui.execute.setEnabled(False)
+            self._ui.menuFile.setEnabled(False)
+
+            self._ui.combo_box_1.setEnabled(True)
+            self._ui.combo_box_2.setEnabled(True)
+            self._ui.combo_box_3.setEnabled(True)
+
+            self.clear_all_results()
+            self.warning_video_taken()
+
+    def show_video_images(self, beam_sum_img):
         """
-        start gif animation in result page
+        When the run button is pressed, it shows the last things that need to be changed on the screen.
         """
-        self._ui.gif_2.setMovie(self.movie2)
+        if self._controller.get_running() == True:
+            
+            self._ui.tabwidget.setCurrentIndex(1)
 
-    def show_thresh_images(self):
-        """
-        Displaying images and values according to the selected threshold value
-        """
-        self._ui.run_video.setEnabled(True)
-        self._ui.clear.setEnabled(True)
-        
-        self._ui.g_image_3.setPixmap(QPixmap.fromImage(self._controller.get_binary_img()))
-        self._ui.g_image_4.setPixmap(QPixmap.fromImage(self._controller.get_binary_img()))
+            self._ui.g_image_1.setPixmap(beam_sum_img)
+            self._ui.g_image_2.setPixmap(beam_sum_img)
 
-        #image1 Threshold Results
-        self._ui.T1_label_3.setText(str(self._controller.get_y_mm()) + 'mm')
-        self._ui.T1_label_4.setText(str(self._controller.get_x_mm()) + 'mm')
-        #image2 Trhreshold Results
-        self._ui.T2_label_3.setText(str(self._controller.get_y_mm())+ 'mm')
-        self._ui.T2_label_4.setText(str(self._controller.get_x_mm())+ 'mm')
-        #error
-        self._ui.ei_label_1.setText(str(self._controller.get_x_mm()))
-        self._ui.ei_label_2.setText(str(self._controller.get_y_mm()))
-        self._ui.ei_label_3.setText(str(self._controller.get_x_mm()))
-        self._ui.ei_label_4.setText(str(self._controller.get_y_mm()))
-        #average anterior error
-        self._ui.ei_label_5.setText(str(self._controller.get_avr_ant_err()))
-        #total target error (pisagor)
-        self._ui.ei_label_6.setText(str(self._controller.get_targetting_err()))
+            self._ui.select_video.setEnabled(True)
+            self._ui.run_video.setEnabled(True)
+            self._ui.stop_video.setEnabled(False)
+            self._ui.execute.setEnabled(True) 
 
-    def show_first_thresh_image(self):
-        """
-        the first frame in the selected video is displayed on the screen
-        """
-        self._ui.player.setPixmap(self._controller.get_first_frame())
+            self._ui.combo_box_1.setEnabled(True)
+            self._ui.combo_box_2.setEnabled(True)
+            self._ui.combo_box_3.setEnabled(True)
 
-    def show_second_image(self):
+    def after_5_sn_img(self, img):
         """
         Displaying the picture where the points are combined with a line after the points determined in the first frame are determined
         """
-        self._ui.player.setPixmap(self._controller.get_second_frame())
+        self._ui.player.setPixmap(img)
+
+    def after_select_thresh(self):
+
+        self._ui.gif.setMovie(self.movie)
+        self.start_gif_animation()
+        self._ui.run_video.setEnabled(True)
+        self._ui.clear.setEnabled(True)
+
+    def show_thresh_images(self, list):
+        """
+        Displaying images and values according to the selected threshold value
+        """
+        self._ui.menuFile.setEnabled(True)
+        self._ui.g_image_3.setPixmap(QPixmap.fromImage(list[0]))
+        self._ui.g_image_4.setPixmap(QPixmap.fromImage(list[0]))
+
+        #image1 Threshold Results
+        self._ui.T1_label_3.setText(str(list[2]) + 'mm')
+        self._ui.T1_label_4.setText(str(list[1]) + 'mm')
+        #image2 Trhreshold Results
+        self._ui.T2_label_3.setText(str(list[2])+ 'mm')
+        self._ui.T2_label_4.setText(str(list[1])+ 'mm')
+        #error
+        self._ui.ei_label_1.setText(str(list[1]))
+        self._ui.ei_label_2.setText(str(list[2]))
+        self._ui.ei_label_3.setText(str(list[1]))
+        self._ui.ei_label_4.setText(str(list[2]))
+        #average anterior error
+        self._ui.ei_label_5.setText(str(list[3]))
+        #total target error (pisagor)
+        self._ui.ei_label_6.setText(str(list[4]))
+
+    def show_first_thresh_image(self, first_img):
+        """
+        the first frame in the selected video is displayed on the screen
+        """
+        self._ui.player.setPixmap(first_img)
         
+    def set_progressbar(self, count):
+        """
+        Written for progress bar but not used
+        """
+        self._ui.progressbar.setValue(count)
+
     def clear_all_results(self):
         """
         Clearing the values on the screen when the clear button is pressed 
@@ -250,23 +233,24 @@ class MainView(QMainWindow):
 
         self._ui.clear.setEnabled(False)
 
-    def warning_video_taken(self):
+    def thread_complete(self):
+        print("THREAD COMPLETE!")
+    
+    def warning(self):
         msg = QMessageBox()
         msg.setIcon(QMessageBox.Warning)
         msg.setWindowTitle("Error")
-        msg.setText("Points not detected, new video should be taken!")
+        msg.setText("Warning!")
 
         x = msg.exec_()
-        self.close_app()
 
-    def warning_division_zero(self):
+    def warning_save_variables(self):
         msg = QMessageBox()
         msg.setIcon(QMessageBox.Warning)
         msg.setWindowTitle("Error")
-        msg.setText("No white space found in the video!")
+        msg.setText("All variables were save!")
 
         x = msg.exec_()
-        self.close_app()
 
     def warning_video(self):
         msg = QMessageBox()
@@ -276,13 +260,51 @@ class MainView(QMainWindow):
 
         x = msg.exec_()
 
-    def warning_path(self):
+    def warning_beam_thresh(self):
         msg = QMessageBox()
         msg.setIcon(QMessageBox.Warning)
         msg.setWindowTitle("Error")
-        msg.setText("Wrong path!")
+        msg.setText("Threshold area is not detected, new video should be taken or decrease beam threshold!")
 
         x = msg.exec_()
+
+    def warning_video_taken(self):
+        msg = QMessageBox()
+        msg.setIcon(QMessageBox.Warning)
+        msg.setWindowTitle("Error")
+        msg.setText("Referans points are not detected, new video should be taken or decrease referans point threshold!")
+
+        x = msg.exec_()
+        self.close_app()
+    
+    def warning_help_about(self):
+        msg = QMessageBox()
+        msg.setIcon(QMessageBox.Information)
+        msg.setWindowTitle("About ImageX Calibration")
+        msg.setText("ImageX Calibration 1.1\n\nCreated by Vivente Soft\nZeynep iskenderoğlu")
+        msg.setStandardButtons(QMessageBox.Ok)
+
+        x = msg.exec_()
+    
+    def stop_gif(self):
+
+        self.stop_gif_animation()
+
+    def warning_msg(self, flag):
+        if flag == 1:
+            self.warning_beam_thresh()
+    
+    def start_gif_animation(self):   
+        """
+        Start Gif Animation
+        """     
+        self.movie.start()
+
+    def stop_gif_animation(self):
+        """
+        Stop Gif Animation
+        """ 
+        self.movie.stop()
 
     def close_app(self):
         reply = QMessageBox.question(self, "Window Close", "Are you sure you want to close the window?",
